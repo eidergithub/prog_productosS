6b554c9f39209d76b8a5701631f8d59722737260
Eider <Eider@DESKTOP-8BK6AMO>
2020-03-31 12:26:43 +0200

    get egin da, id-a eman eta produktuaren datu guztiak pasatzen ditu

--- src/modelo/dao/ModeloProducto.java
+++ src/modelo/dao/ModeloProducto.java
@@ -40,7 +40,31 @@ public class ModeloProducto extends Conector {
 	}
 	
 	public Producto get(int id) {
-		//TODO implementar
+		
+		PreparedStatement pst;
+		try {
+			pst = super.conexion.prepareStatement("select * from productos where id=?");
+			pst.setInt(1, id);
+			ResultSet rs = pst.executeQuery();
+			
+			if(rs.next()) {
+				Producto producto = new Producto();
+				producto.setId(rs.getInt("id"));
+				producto.setNombre(rs.getString("nombre"));
+				producto.setStock(rs.getInt("stock"));
+				producto.setFecha_compra(rs.getDate("fecha_compra"));
+				producto.setColor(rs.getString("color"));
+				producto.setMade_in(rs.getString("made_in"));
+				producto.setPrecio(rs.getDouble("precio"));
+				producto.setDescuento(rs.getInt("descuento"));
+				producto.setTallas(rs.getString("tallas"));
+				
+				return producto;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 		return null;
 	}
 	
